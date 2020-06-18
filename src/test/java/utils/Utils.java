ab901ad7ad245c5260cf7ffbf560f369e4618fc9
Lenovo <77ripdrive@gmail.com>
2020-06-19 01:44:50 +0300

    simple refactoring

--- src/test/java/utils/Utils.java
+++ src/test/java/utils/Utils.java
@@ -2,6 +2,8 @@ package utils;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -10,7 +12,6 @@ import java.io.IOException;
 import java.util.Map;
 
 public class Utils {
-
 
     public Map setCoordinates(double latitude, double longitude) {
         return Map.of(
@@ -23,16 +24,22 @@ public class Utils {
     public void getScreenshot(By by, String country, WebDriver driver) throws IOException {
         WebElement allProvider = driver.findElement(by);
         File file = allProvider.getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(file, new File("screenshots/provider"+country+".png" ));
-
+        FileUtils.copyFile(file, new File("screenshots/provider" + country + ".png"));
     }
 
-    public Wait waitForElement(WebDriver driver, int timeoutSeconds) {
-        return new WebDriverWait(driver, timeoutSeconds);
+    public WebElement waitForElementVisibility(WebDriver driver, int timeoutSeconds, By by) {
+        return new WebDriverWait(driver, timeoutSeconds)
+                .until(ExpectedConditions.visibilityOf(driver.findElement(by)));
     }
 
-    public void screenScroll(WebDriver driver){
+    public void screenScroll(WebDriver driver) {
         JavascriptExecutor jse = ((JavascriptExecutor) driver);
         jse.executeScript("window.scrollTo(0, 400)");
+    }
+
+    public void setProvider(Actions actions, WebDriver driver, By allProviders, By provider) {
+        actions.moveToElement(driver.findElement(allProviders))
+                .moveToElement(driver.findElement(provider))
+                .click().build().perform();
     }
 }
